6271a18f23b3dac63b0b7fbc8e609e9c7992e205
muthu-pencilcard <muthupencilcard@gmail.com>
2026-04-30 20:22:07 +1000

    cmit hello signin

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,4 +1,4 @@
-import { defineAuth } from '@aws-amplify/backend';
+import { defineAuth, secret } from '@aws-amplify/backend';
 import { postConfirmationFn } from '../functions/post-confirmation/resource';
 
 export const auth = defineAuth({
@@ -8,6 +8,36 @@ export const auth = defineAuth({
       verificationEmailSubject: 'Welcome to BeboCard!',
       verificationEmailBody: (createCode) =>
         `Your BeboCard verification code is ${createCode()}`,
+    },
+    externalProviders: {
+      google: {
+        clientId: secret('GOOGLE_CLIENT_ID'),
+        clientSecret: secret('GOOGLE_CLIENT_SECRET'),
+        scopes: ['email', 'profile'],
+        attributeMapping: {
+          email: 'email',
+          fullname: 'name',
+        },
+      },
+      signInWithApple: {
+        clientId: secret('SIWA_CLIENT_ID'),
+        keyId: secret('SIWA_KEY_ID'),
+        privateKey: secret('SIWA_PRIVATE_KEY'),
+        teamId: secret('SIWA_TEAM_ID'),
+        scopes: ['email', 'name'],
+        attributeMapping: {
+          email: 'email',
+          fullname: 'name',
+        },
+      },
+      callbackUrls: [
+        'bebocard://callback/',
+        'https://bebocard.com.au/callback/',
+      ],
+      logoutUrls: [
+        'bebocard://signout/',
+        'https://bebocard.com.au/signout/',
+      ],
     },
   },
   multifactor: {
